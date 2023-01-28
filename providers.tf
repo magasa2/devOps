4aef6e0a3e085d40940bac075419930eefd61472
Maria Garcia Sancho <garciasancho17@gmail.com>
2023-01-28 13:18:15 +0100

    11111

---
+++ providers.tf
@@ -0,0 +1,13 @@
+provider "google" {
+    project = var.project_id
+    region = var.region
+    zone = var.zone
+}
+
+terraform {
+    backend "gcs"{
+        bucket = "mariaedem"
+        prefix = "terraforms/state"
+    }
+
+}
